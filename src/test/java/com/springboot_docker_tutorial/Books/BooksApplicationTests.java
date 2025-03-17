baf42783266434e730fe22cf73b61e42233dd511
Kavin <kavin1615yoga@gmail.com>
2025-03-17 16:01:25 +0530

    Inital commit

---
+++ src/test/java/com/springboot_docker_tutorial/Books/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_docker_tutorial.Books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
